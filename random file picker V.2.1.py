fa10e381496f4ccf302ba40cdc6607a875614995
fukkaer <98552446+fukkaer@users.noreply.github.com>
2022-01-28 04:09:52 +0200

    Add files via upload

---
+++ random file picker V.2.1.py
@@ -0,0 +1,52 @@
+import os, random
+import webbrowser
+
+
+def filepicker():
+    basedir = 'C\\Users'
+    try:
+        userinput01 = input('Input the directory to use: ')
+        try:
+            userinput01_stripped = userinput01.strip('"')
+            basedir = userinput01_stripped
+        except Exception as b:
+            print(b)
+    except Exception as c:
+        print(c)
+    loop = True
+    while loop == True:
+        print('\n\nType dir to change current directory.\n\n')
+        userinput02 = input('open random file? Y/N: ')
+        if userinput02 == 'Y' or userinput02 == 'y':
+            try:
+                file = random.choice([x for x in os.listdir(basedir) if os.path.isfile(os.path.join(basedir, x))])
+                print('Opening Random File {}...'.format(file))
+                webbrowser.open(os.path.join(basedir, file))
+            except Exception as d:
+                print(d)
+        elif userinput02 == 'n' or userinput02 == 'N':
+            try:
+                loop = False
+                break
+            except Exception as e:
+                print(e)
+        elif userinput02 =='dir'or userinput02 == 'directory' or userinput02 == 'basedir' or userinput02 == 'basedirectory' or userinput02 == 'base directory':
+            try:
+                userinput03 = input('Input the new directory to use: ')
+                try:
+                    userinput03_stripped = userinput03.strip('"')
+                    basedir = userinput03_stripped
+                except Exception as f:
+                    print(f)
+            except Exception as g:
+                print(g)
+        else:
+            print('Select valid option')
+def main():
+    print('Random File Picker V.2')
+    try:
+        filepicker()
+    except Exception as h:
+        print(h)
+if __name__ == '__main__':
+    main()
